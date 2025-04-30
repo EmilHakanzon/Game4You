08c38c29863d2fdf5517060a19b35e61fab594b6
Emil Johansson <Emilaik@gmail.com>
2025-04-30 11:23:30 +0200

    Removed dropdown for search...

--- api-project-f24/src/ui/searchbar.tsx
+++ api-project-f24/src/ui/searchbar.tsx
@@ -17,28 +17,7 @@ export const SearchBar = () => {
 
   return (
     <div className="flex flex-col sm:flex-row items-center sm:items-end gap-4 w-full relative pl-[200px]">
-      {/* Filter Dropdown */}
-      <div className="relative">
-        <button
-          onClick={() => setShowFilter((prev) => !prev)}
-          className="text-[#3B82F6] cursor-pointer hover:bg-[#1b2435] transition"
-        >
-          <ChevronDown size={16} />
-        </button>
-        {showFilter && (
-          <div className="absolute top-full left-0 mt-2 bg-purple-800 text-white rounded shadow-md z-10 min-w-[150px]">
-            {dropdownOptions.map((option) => (
-              <button
-                key={option}
-                onClick={() => handleSelect(option)}
-                className="block w-full text-left px-4 py-2 hover:bg-purple-600"
-              >
-                {option}
-              </button>
-            ))}
-          </div>
-        )}
-      </div>
+  
       {/* Input */}
       <div className="flex flex-col w-full max-w-md">
         <input
