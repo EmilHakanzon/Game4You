ffb7e1282704b1ec237447f9e45bab06cef9b2d9
Emil Johansson <Emilaik@gmail.com>
2025-05-12 21:09:05 +0200

    fix

--- api-project-f24/src/components/nav/mobileMenu.tsx
+++ api-project-f24/src/components/nav/mobileMenu.tsx
@@ -12,26 +12,27 @@ export default function MobileMenu({ isOpen, pathname }: MobileMenuProps) {
   if (!isOpen) return null;
 
   return (
-    <div className="md:hidden bg-[#1E293B] -mt-2 transition-all duration-300 opacity-80 p-4">
+    <div className="md:hidden bg-[#1E293B] -mt-2 transition-all duration-300 opacity-90 p-4">
       <div className="flex flex-col space-y-4">
-        {/* Left: Dropdown and Avatar */}
-        <div className="flex items-center justify-between">
-          <div className="flex items-center space-x-2">
-            <DropDown />
-            <img
-              src="avatar.png"
-              alt="gamingavatar"
-              className="object-contain w-12 h-12 rounded-full"
-            />
-          </div>
-        </div>
+        {/* Row: Dropdown, Avatar, and SearchBar */}
+        <div className="flex items-center justify-between space-x-4">
+          {/* Dropdown */}
+          <DropDown />
 
-        {/* Right: SearchBar */}
-        {pathname === "/" && (
-          <div className="w-full">
-            <SearchBar />
-          </div>
-        )}
+          {/* Avatar */}
+          <img
+            src="avatar.png"
+            alt="gamingavatar"
+            className="object-contain w-12 h-12 rounded-full"
+          />
+
+          {/* SearchBar */}
+          {pathname === "/" && (
+            <div className="flex-1">
+              <SearchBar />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
